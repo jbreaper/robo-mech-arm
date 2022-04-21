3af24ae82250b220147e75b8a52d2311d31e5b86
jbreaper <jbmavor@hotmail.co.uk>
2022-04-21 12:58:41 +0100

    continious servo alterations

--- arm/arm.ino
+++ arm/arm.ino
@@ -1,30 +1,42 @@
 #include <cppQueue.h>
-#include <ESP32Servo.h>
 #include <Ticker.h>
+#include <ESP32Servo.h>
 #include <ServoEasing.hpp>
 #include <analogWrite.h>
 #include <ESP32Tone.h>
 #include <ESP32PWM.h>
 
-#define J1 10
-#define J2 11
-#define J3 12
-#define J4 13
-#define J5 14
+#define J1 32
+#define J2 33
+#define J3 25
+#define J4 26
+#define J5 27
+#define S1 12
 
-const int joint_out[5] = {J1, J2, J3, J4, J5};
+#define J1_Rate 90    // deg/s
+#define J1_CW   77
+#define J1_CCW  109.5
+
+#define J5_Rate 180   // deg/s
+#define J5_CW   76.9
+#define J5_CCW  109
+
+int joint_out[5] = {J1, J2, J3, J4, J5};
 int ini_angle[5] = {45, 45, 45, 45, 45};
 
-#define S1 15
+int j1_cur = 90;
+int j5_cur = 90;
 
-#define ESO 32
-#define ESI 31
+#define ESO 15
+#define ESI 16
 
-ServoEasing joint1;
+Servo joint1;
 ServoEasing joint2;
 ServoEasing joint3;
 ServoEasing joint4;
-ServoEasing joint5;
+Servo joint5;
+
+Servo gripper;
 
 int angle[5];
 int coord[3];
@@ -33,7 +45,8 @@ int pos = 0;
 
 bool smooth = true;
 bool grab = false;
-bool grabbed = false; 
+bool grabbed = false;
+bool first = true;
 
 volatile bool emergancy_stop = false;
 
@@ -41,23 +54,59 @@ cppQueue cmds(sizeof(String), 7, FIFO, false);
 
 void setup()
 {
+	Serial.begin(9600);
+
+	Serial.println("booting: begin");
 	pinMode(ESO, OUTPUT);
 	digitalWrite(ESO, HIGH);
 	pinMode(ESI, INPUT);
 	attachInterrupt(ESI, e_stop, HIGH);
 
-	pinMode(S1, OUTPUT);
+  gripper.attach(S1);
+//  gripper.setEasingType(EASE_QUADRATIC_IN_OUT);
 
-	Serial.begin(9600);
-	for (int i = 0; i < 6; i++)
+	Serial.println("booting: pins assigned");
+	for (int i = 0; i < 5; i++)
 	{
-		ServoEasing::ServoEasingArray[i]->attach(joint_out[i], ini_angle[i]);
-		ServoEasing::ServoEasingArray[i]->setEasingType(EASE_QUADRATIC_IN_OUT);
+		switch (i)
+		{
+		case 0:
+			joint1.attach(joint_out[i]);
+			break;
+		case 1:
+			joint2.attach(joint_out[i]);
+			joint2.setEasingType(EASE_QUADRATIC_IN_OUT);
+			break;
+		case 2:
+			joint3.attach(joint_out[i]);
+			joint3.setEasingType(EASE_QUADRATIC_IN_OUT);
+			break;
+		case 3:
+			joint4.attach(joint_out[i]);
+			joint4.setEasingType(EASE_QUADRATIC_IN_OUT);
+			break;
+		case 4:
+			joint5.attach(joint_out[i]);
+			break;
+		default:
+			break;
+		}
+		// ServoEasing::ServoEasingArray[i]->attach(joint_out[i], ini_angle[i]);
+		// ServoEasing::ServoEasingArray[i]->setEasingType(EASE_QUADRATIC_IN_OUT);
 	}
+
+	Serial.println("booting: servos assigned");
 
 	while (!Serial)
 	{
 	}
+
+ 
+  joint2.write(85);
+  joint3.write(85);
+  joint4.write(85);
+
+  delay(2000);
 }
 
 // interupt function used to stop arm in case of emergancy
@@ -71,24 +120,71 @@ void e_stop()
 //
 // }
 
+void j1_move(float angle){
+  float x = 0;
+  
+  if((j1_cur-angle) > 0){
+    x = (j1_cur-angle);
+    joint1.write(J1_CW);
+  } else if ((j1_cur-angle) < 0){
+    x = (angle-j1_cur);
+    joint1.write(J1_CCW);    
+  } else if ((j1_cur-angle) == 0){
+    return;
+  }
+
+  delay(1000*(x/J1_Rate));
+  joint1.write(90);
+  j1_cur = angle;
+}
+
+void j5_move(float angle){
+  float x = 0;
+  
+  if((j5_cur-angle) > 0){
+    x = (j5_cur-angle);
+    joint5.write(J5_CW);
+  } else if ((j5_cur-angle) < 0){
+    x = (angle-j5_cur);
+    joint5.write(J5_CCW);    
+  } else if ((j5_cur-angle) == 0){
+    return;
+  }
+
+  delay(1000*(x/J5_Rate));
+  joint2.write(90);
+  j5_cur = angle;
+}
+
+// theta = degreeX10
 bool joint_to(int theta[5], int rate = 90)
 {
 	for (int i = 0; i < 6; i++)
 	{
-		ServoEasing::ServoEasingArray[i]->easeTo(theta[i], rate);
+		switch (i)
+		{
+		case 0:
+			j1_move(theta[i]/10);
+			break;
+		case 1:
+			joint2.easeTo((theta[i]/10), rate);
+			break;
+		case 2:
+			joint3.easeTo((theta[i]/10), rate);
+			break;
+		case 3:
+			joint4.easeTo((theta[i]/10), rate);
+			break;
+		case 4:
+			j5_move(theta[i]/10);
+			break;
+		}
+		// ServoEasing::ServoEasingArray[i]->easeTo(theta[i], rate);
 	}
 }
 
 void joint_reset()
 {
-	int zeroes[5] = {
-		0,
-		0,
-		0,
-		0,
-		0,
-	};
-
 	joint_to(ini_angle, 180);
 }
 
@@ -269,32 +365,67 @@ void reply(bool out_msg)
 {
 	if (true)
 	{
-		Serial.print("Move: successful");
+		Serial.println("Move: successful");
 	}
 	else
 	{
-		Serial.print("Move: failed");
+		Serial.println("Move: failed");
 	}
 }
 
 void loop()
 {
-	if (Serial.available() > 0)
-	{
-		input();
-	}
+	//	if (Serial.available() > 0)
+	//	{
+	//		input();
+	//	}
 
-	interperate();
+	//	interperate();
 
-	joint_to(angle);
+//	joint_to(angle);
+//
+//	joint_reset();
 
-	if (grabbed != grab)
-	{
-		grab = selinoid(grabbed);
-	}
+  int x = 110-90;
 
-	if (cmds.isEmpty())
-	{
-		reply(true);
-	}
+  
+//  int x = 110-90;
+//  joint5.write(109);
+//  delay(1000);
+//  joint5.write(90);
+//  delay(1000);
+//  joint5.write(76.9);
+//  delay(1000);
+//  joint5.write(90);
+//  delay(2000);
+
+  if(first){
+    joint2.easeTo(70, 20);
+  
+    joint2.easeTo(80, 20);
+    joint2.write(80);
+    delay(500);
+   first = false;
+  }
+  joint4.easeTo(135, 25);
+  joint3.easeTo(45, 25);
+  delay(2000);
+  joint4.easeTo(45, 25);
+  joint3.easeTo(135, 25);
+  delay(2000);
+
+//  joint1.write(77);
+//  delay(1000);
+//  joint1.write(90+(1.5*(90-77)));
+//  delay(1000);
+
+	//	if (grabbed != grab)
+	//	{
+	//		grab = selinoid(grabbed);
+	//	}
+	//
+	//	if (cmds.isEmpty())
+	//	{
+	//		reply(true);
+	//	}
 }
